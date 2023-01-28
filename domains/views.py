48aca728fccaa38343a5c248d386a56fe71c5f36
Kevin Morris <kevr@0cost.org>
2023-01-27 17:53:02 -0800

    fix: catch exceptions in /domains/
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- domains/views.py
+++ domains/views.py
@@ -1,9 +1,11 @@
 """Copyright (C) 2023 Kevin Morris"""
 import json
+from http import HTTPStatus
 from urllib.parse import quote_plus
 
 import requests_unixsocket
 from django.conf import settings
+from requests.exceptions import ConnectionError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -17,8 +19,19 @@ class DomainsView(APIView):
         user = request.user.username
         session = requests_unixsocket.Session()
         uri = quote_plus(settings.WEBVIRTD_SOCKET)
-        response = session.post(
-            f"http+unix://{uri}/domains",
-            data=json.dumps({"user": user}),
-        )
-        return Response(response.json())
+
+        status_code = HTTPStatus.OK
+        data = {}
+        try:
+            response = session.post(
+                f"http+unix://{uri}/domains",
+                data=json.dumps({"user": user}),
+            )
+
+            status_code = response.status_code
+            data = response.json()
+        except ConnectionError:
+            status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+            data["detail"] = "Unable to connect to webvirtd"
+
+        return Response(data, status=status_code)
