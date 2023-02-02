fd1defddbc95acc94f8cb15ac40f84ee860bf070
Kevin Morris <kevr@0cost.org>
2023-02-02 12:10:22 -0800

    change: convert to using Apache 2.0 styled license comments
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- webvirt_api/wsgi.py
+++ webvirt_api/wsgi.py
@@ -5,8 +5,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-
-Copyright (C) 2023 Kevin Morris
 """
 
 import os
