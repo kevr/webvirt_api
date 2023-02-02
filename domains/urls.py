efde275940b0a969a0b0f8dd6e7e6582c4cf0585
Kevin Morris <kevr@0cost.org>
2023-02-02 01:06:27 -0800

    feat: add /domains/(name)/{start,shutdown}/ routes
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- domains/urls.py
+++ domains/urls.py
@@ -1,9 +1,11 @@
 """Copyright (C) 2023 Kevin Morris"""
 from django.urls import path
 
-from .views import DomainsView, DomainView
+from .views import DomainsView, DomainView, domain_shutdown, domain_start
 
 urlpatterns = [
     path("domains/", DomainsView.as_view()),
     path("domains/<str:name>/", DomainView.as_view()),
+    path("domains/<str:name>/start/", domain_start),
+    path("domains/<str:name>/shutdown/", domain_shutdown),
 ]
--- domains/views.py
+++ domains/views.py
@@ -6,6 +6,8 @@ from urllib.parse import quote_plus
 import requests_unixsocket
 from django.conf import settings
 from requests.exceptions import ConnectionError
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes as permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -60,3 +62,51 @@ class DomainView(APIView):
             data["detail"] = "Unable to connect to webvirtd"
 
         return Response(data, status_code)
+
+
+@api_view(["POST"])
+@permissions([IsAuthenticated])
+def domain_start(request: Request, name: str) -> Response:
+    user = request.user.username
+    session = requests_unixsocket.Session()
+    uri = quote_plus(settings.WEBVIRTD_SOCKET)
+
+    status_code = HTTPStatus.OK
+    data = {}
+    try:
+        response = session.post(
+            f"http+unix://{uri}/domains/{name}/start/",
+            data=json.dumps({"user": user}),
+        )
+
+        status_code = response.status_code
+        data = response.json()
+    except ConnectionError:
+        status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+        data["detail"] = "Unable to connect to webvirtd"
+
+    return Response(data, status_code)
+
+
+@api_view(["POST"])
+@permissions([IsAuthenticated])
+def domain_shutdown(request: Request, name: str) -> Response:
+    user = request.user.username
+    session = requests_unixsocket.Session()
+    uri = quote_plus(settings.WEBVIRTD_SOCKET)
+
+    status_code = HTTPStatus.OK
+    data = {}
+    try:
+        response = session.post(
+            f"http+unix://{uri}/domains/{name}/shutdown/",
+            data=json.dumps({"user": user}),
+        )
+
+        status_code = response.status_code
+        data = response.json()
+    except ConnectionError:
+        status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+        data["detail"] = "Unable to connect to webvirtd"
+
+    return Response(data, status_code)
